2b53410da82a5d4f8644f05c4b27a92c5123e502
Alireza <alireza.shams2106@gmail.com>
2025-08-24 17:22:28 +0330

    dewewfw

--- style/app.js
+++ style/app.js
@@ -1,12 +1,21 @@
-const getData = async () => {
+const getData = async (city) => {
     try {
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=1263413d5c995d8016261a77ed19bd33&units=metric`)
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1263413d5c995d8016261a77ed19bd33&units=metric`)
     const response = await data.json()
     console.log(response)
+    showData(response)
+
     } catch (err) {
         console.log(err);
-        
     }
 }
 
 getData()
+
+function showData(data){
+    const card = document.querySelector('.card-1')
+    const city = card.childNodes[3]
+    const temp = card.lastElementChild
+    document.querySelector(".input")
+}   
+getData(cityName)
