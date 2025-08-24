106f73dc3f77cf575ee28c86f7b3ec376f7ae445
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-08-24 16:26:49 +0330

    🐛 api fixed

--- style/app.js
+++ style/app.js
@@ -1,5 +1,7 @@
 const getData = async () => {
-    const data = fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=10&appid=${APIkey}`)
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=1263413d5c995d8016261a77ed19bd33&units=metric`)
     const response = await data.json
     console.log(response)
 }
+
+getData()
