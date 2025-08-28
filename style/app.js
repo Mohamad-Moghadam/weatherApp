a123614018268354b7146352fa909e1ba404ab2d
Alireza <alireza.shams2106@gmail.com>
2025-08-28 10:01:47 +0330

    refactor: :rocket: Weather api fix

--- style/app.js
+++ style/app.js
@@ -1,21 +1,34 @@
-const getData = async (city) => {
+const getData = async (city, day) => {
     try {
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1263413d5c995d8016261a77ed19bd33&units=metric`)
-    const response = await data.json()
-    console.log(response)
-    showData(response)
-
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=1263413d5c995d8016261a77ed19bd33&units=metric`)
+        const data = await res.json()
+        return data
     } catch (err) {
         console.log(err);
     }
 }
 
-getData()
+function showData(card, forecast, dayIndex){
+    const dailyData = forecast.list.filter((_, i) => i % 6 === 0)
+    const data = dailyData[dayIndex]
+    const city = card.children[1]
+    const tempfield = card.lastElementChild
+    tempfield.textContent = `${data.main.temp} °C`
+    city.textContent = forecast.city.name
+}
 
-function showData(data){
-    const card = document.querySelector('.card-1')
-    const city = card.childNodes[3]
-    const temp = card.lastElementChild
-    document.querySelector(".input")
-}   
-getData(cityName)
+async function main(cityinput){
+    const forecast = await getData(cityinput)
+    const allCards = [...document.querySelectorAll('.card-1')]
+    allCards.forEach((val, index) => {
+        showData(val, forecast, index)
+    })
+}
+
+const btn = document.querySelector(".btn")
+const inp = document.querySelector(".input")
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
+    const input = inp.value
+    main(input)
+})
